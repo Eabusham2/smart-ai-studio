779ca501100d83c98c4df84a5438919307b4119c
Eabusham2 <eabusham2@gmail.com>
2026-09-11 22:02:44 -0500

    fix(core): install learned-adapter restore before MLX locking

--- core/__init__.py
+++ core/__init__.py
@@ -4,10 +4,12 @@ from core.pro_engine import ProReasoningEngine
 from core.mlx_engine import MLXReasoningBackend
 from core.awake_auto_hook import install_awake_auto_learning
 from core.pro_runtime_hardening import install_pro_runtime_hardening
+from core.mlx_adapter_persistence import install_mlx_adapter_persistence
 from core.mlx_runtime_lock import install_mlx_runtime_lock
 from core.speculative_engine import SpeculativeEngine, PromptLookupDrafter, LookaheadJacobiDrafter
 from core.platform import PlatformRouter, get_platform_router, detect_hardware
 
+install_mlx_adapter_persistence(MLXReasoningBackend)
 install_mlx_runtime_lock(MLXReasoningBackend)
 install_awake_auto_learning(ProReasoningEngine)
 install_pro_runtime_hardening(ProReasoningEngine)
