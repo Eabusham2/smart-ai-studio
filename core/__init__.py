9a5bb6db56cc9326587a3e4d745981174f166f2e
Eabusham2 <eabusham2@gmail.com>
2026-09-11 19:35:40 -0500

    Enable awake auto-learning for Pro engine imports

--- core/__init__.py
+++ core/__init__.py
@@ -1,8 +1,11 @@
 from core.entropy_router import EntropyRouter
 from core.verifier import GroundTruthVerifier, VerificationResult
 from core.pro_engine import ProReasoningEngine
+from core.awake_auto_hook import install_awake_auto_learning
 from core.speculative_engine import SpeculativeEngine, PromptLookupDrafter, LookaheadJacobiDrafter
 from core.platform import PlatformRouter, get_platform_router, detect_hardware
+
+install_awake_auto_learning(ProReasoningEngine)
 
 __all__ = [
     "EntropyRouter",
@@ -16,4 +19,3 @@ __all__ = [
     "get_platform_router",
     "detect_hardware",
 ]
-
