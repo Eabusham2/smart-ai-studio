b7dd8975d8fc613ae9eb2f7b73ed4a0a1c401f67
Eabusham2 <eabusham2@gmail.com>
2026-09-16 22:25:02 -0500

    Wire lossless full-context chat hardening

--- core/__init__.py
+++ core/__init__.py
@@ -3,6 +3,7 @@ from core.verifier import GroundTruthVerifier, VerificationResult
 from core.pro_engine import ProReasoningEngine
 from core.mlx_engine import MLXReasoningBackend
 from core.awake_auto_hook import install_awake_auto_learning
+from core.full_context_hardening import install as install_full_context_hardening
 from core.pro_runtime_hardening import install_pro_runtime_hardening
 from core.mlx_adapter_persistence import install_mlx_adapter_persistence
 from core.mlx_runtime_lock import install_mlx_runtime_lock
@@ -12,6 +13,7 @@ from core.platform import PlatformRouter, get_platform_router, detect_hardware
 install_mlx_adapter_persistence(MLXReasoningBackend)
 install_mlx_runtime_lock(MLXReasoningBackend)
 install_awake_auto_learning(ProReasoningEngine)
+install_full_context_hardening(ProReasoningEngine)
 install_pro_runtime_hardening(ProReasoningEngine)
 
 __all__ = [
