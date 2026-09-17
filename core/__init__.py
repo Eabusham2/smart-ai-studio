62f8f8f02e8e8404593f124501f529b9148dd526
Eabusham2 <eabusham2@gmail.com>
2026-09-16 20:54:18 -0500

    Wire existing Pro synthesis into chat runtime

--- core/__init__.py
+++ core/__init__.py
@@ -4,6 +4,7 @@ from core.pro_engine import ProReasoningEngine
 from core.mlx_engine import MLXReasoningBackend
 from core.awake_auto_hook import install_awake_auto_learning
 from core.pro_runtime_hardening import install_pro_runtime_hardening
+from core.pro_model_synthesis import install_pro_model_synthesis
 from core.mlx_adapter_persistence import install_mlx_adapter_persistence
 from core.mlx_runtime_lock import install_mlx_runtime_lock
 from core.speculative_engine import SpeculativeEngine, PromptLookupDrafter, LookaheadJacobiDrafter
@@ -13,6 +14,7 @@ install_mlx_adapter_persistence(MLXReasoningBackend)
 install_mlx_runtime_lock(MLXReasoningBackend)
 install_awake_auto_learning(ProReasoningEngine)
 install_pro_runtime_hardening(ProReasoningEngine)
+install_pro_model_synthesis(ProReasoningEngine)
 
 __all__ = [
     "EntropyRouter",
