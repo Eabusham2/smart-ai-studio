6900a1cdc5d9ae77984103a5ecc95752624f83c1
Eabusham2 <eabusham2@gmail.com>
2026-09-16 23:38:17 -0500

    Wire ternary temperature policy into chat Pro engine

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,4 @@
+import core.pro_engine as pro_engine_module
 from core.entropy_router import EntropyRouter
 from core.verifier import GroundTruthVerifier, VerificationResult
 from core.pro_engine import ProReasoningEngine
@@ -8,12 +9,14 @@ from core.pro_runtime_hardening import install_pro_runtime_hardening
 from core.mlx_adapter_persistence import install_mlx_adapter_persistence
 from core.mlx_runtime_lock import install_mlx_runtime_lock
 from core.unified_context_budget import install as install_unified_context_budget
+from core.temperature_policy import install_chat as install_chat_temperature_policy
 from core.speculative_engine import SpeculativeEngine, PromptLookupDrafter, LookaheadJacobiDrafter
 from core.platform import PlatformRouter, get_platform_router, detect_hardware
 
 install_mlx_adapter_persistence(MLXReasoningBackend)
 install_mlx_runtime_lock(MLXReasoningBackend)
 install_unified_context_budget(MLXReasoningBackend)
+install_chat_temperature_policy(pro_engine_module)
 install_awake_auto_learning(ProReasoningEngine)
 install_full_context_hardening(ProReasoningEngine)
 install_pro_runtime_hardening(ProReasoningEngine)
